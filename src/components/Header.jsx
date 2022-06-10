9c2a3f5b3073b7105befe7a856f663db11e1a357
Cheekyprogrammer <cheekyprogrammer@gmail.com>
2022-06-10 19:16:52 +0000

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import NoteIcon from '@mui/icons-material/Note';
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <NoteIcon /> Cheeky Keeper
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
